b34c4d0b97991940b8fb5460e568204b1e666efa
Kirill Sakovich <kirill.s.sakovich@gmail.com>
2021-05-12 18:02:51 -0700

    DB: add new item from website

--- models/product.js
+++ models/product.js
@@ -4,12 +4,16 @@ const dbFetcher = new DbFetcher();
 
 module.exports = class Product
 {
-  constructor(id, title, imageUrl, description, price)
+  constructor(id, title, category_id, vendor_id, imageUrl, description, is_composite, unit, price)
   {
     this.id = id;
+    this.category_id = category_id;
     this.title = title;
+    this.vendor_id = vendor_id;
     this.imageUrl = imageUrl;
     this.description = description;
+    this.is_composite = is_composite;
+    this.unit = unit;
     this.price = price;
   }
 
