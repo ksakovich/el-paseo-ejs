a581fb7ce635da6300b2f35e7bfc92e05a258790
Kirill Sakovich <kirill.s.sakovich@gmail.com>
2021-05-19 00:34:11 -0700

    Fixed update product (admin pages and controllers)

--- controllers/admin.js
+++ controllers/admin.js
@@ -57,8 +57,9 @@ exports.getEditProduct = (req, res, next) =>
     return res.redirect('/');
   }
   const prodId = req.params.productId;
+
   // findByPk()
-  dbFetcher.findById(prodId, product =>
+  Product.findByPk(prodId).then(product =>
   {
     if (!product)
     {
@@ -70,66 +71,66 @@ exports.getEditProduct = (req, res, next) =>
       editing: editMode,
       product: product
     });
+  }).catch(err =>
+  {
+    console.log(err);
   });
 };
 
 exports.postEditProduct = (req, res, next) =>
 {
-  // const title = req.body.title;
-  // const categoryId = req.body.category_id;
-  // const vendorId = req.body.vendor_id;
-  // const imageUrl = req.body.imageUrl;
-  // const description = req.body.description;
-  // const isComposite = req.body.is_composite;
-  // const unit = req.body.unit;
-  // const price = req.body.price;
-  // const product = new Product(
-  //   null, 
-  //   title, 
-  //   categoryId, 
-  //   vendorId, 
-  //   imageUrl,
-  //   description, 
-  //   isComposite, 
-  //   unit, 
-  //   price);
-  // dbFetcher.insertNewItem(product);
-
-
-  const prodId = req.body.productId;
-  const updatedTitle = req.body.title;
+  const prodId = req.body.product_id;
   const updatedCategoryId = req.body.category_id;
+  const updatedTitle = req.body.title;
   const updatedVendorId = req.body.vendor_id;
+  const updatedShortDescription = req.body.short_description;
+  const updatedLongDescription = req.body.long_description;
+  const updatedSmallImageUrl = req.body.small_image_url;
+  const updatedBigImageUrl = req.body.big_image_url;
   const updatedPrice = req.body.price;
-  const updatedImageUrl = req.body.imageUrl;
-  const updatedDesc = req.body.description;
-  const updatedIsComposite = req.body.is_composite;
+  const updatedIsComposite = req.body.is_composite ? true : false;
   const updatedUnit = req.body.unit;
-  const updatedProduct = new Product(
-    prodId,
-    updatedTitle,
-    updatedCategoryId,
-    updatedVendorId,
-    updatedImageUrl,
-    updatedDesc,
-    updatedIsComposite,
-    updatedUnit,
-    updatedPrice
-  );
-  updatedProduct.save();
+  const updatedQuantity = req.body.quantity_in_stock;
+
+  Product.findByPk(prodId).then(product =>
+  {
+    product.product_id = prodId;
+    product.category_id = updatedCategoryId;
+    product.title = updatedTitle;
+    product.vendor_id = updatedVendorId;
+    product.short_description = updatedShortDescription;
+    product.long_description = updatedLongDescription;
+    product.small_imageUrl = updatedSmallImageUrl;
+    product.big_image_url = updatedBigImageUrl;
+    product.price = updatedPrice;
+    product.is_composite = updatedIsComposite;
+    product.unit = updatedUnit;
+    product.quantity_in_stock = updatedQuantity;
+    return product.save();
+  }).then(result =>
+  {
+    console.log(`Updated product in DB: ${updatedTitle}`);
+  }).catch(err =>
+  {
+    console.log(err);
+  });
+
   res.redirect('/admin/products');
 };
 
 exports.getProducts = (req, res, next) =>
 {
-  Product.fetchAll(products =>
+  Product.findAll().then(products =>
   {
     res.render('admin/products', {
       prods: products,
       pageTitle: 'Admin Products',
       path: '/admin/products'
     });
-  });
+  }).catch(err =>
+  {
+    console.log(err);
+  })
 };
 
 exports.postDeleteProduct = (req, res, next) =>
--- controllers/shop.js
+++ controllers/shop.js
@@ -13,7 +13,6 @@ exports.getProducts = (req, res, next) =>
   Product.findAll()
     .then((products) =>
     {
-      console.log(products);
       res.render("shop/product-list", {
         prods: products,
         pageTitle: "All Products",
